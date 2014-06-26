48041a33915d2157eacd4c458c7112a006645c91
Will Beasley <wibeasley@hotmail.com>
2014-06-26 01:15:48 -0500

    Added static HTML table

--- PhpProject2/index.php
+++ PhpProject2/index.php
@@ -14,5 +14,19 @@ and open the template in the editor.
         <?php
         // put your code here
         ?>
+     <p>
+     Expected properties are:
+     <ul>
+       <li>id</li>
+       <li>priority</li>
+       <li>created_on</li>
+       <li>due_on</li>
+       <li>last_modified_on</li>
+       <li>title</li>
+       <li>description</li>
+       <li>comment</li>
+       <li>status</li>
+       <li>deleted</li>
+     </ul>
     </body>
 </html>
