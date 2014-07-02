e2be6c630036549430d65b0ff1050a1d4c368505
Will Beasley <wibeasley@hotmail.com>
2014-07-02 01:42:04 -0500

    moving around and removing scraps

--- PhpProject2/index.php
+++ PhpProject2/index.php
@@ -1,23 +1,33 @@
 <!DOCTYPE html>
+----
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<?php 
-    $db = new mysqli("localhost", "phpTest", "o", "redcap");
-    if($db->connect_error) {
-        die("Connect Error ({$db->connect_errno}){$db->connect_error}");
-    }
-    $sql = "SELECT project_id, project_name, app_title FROM redcap.redcap_projects;";
-    $result = $db->query($sql);
-?>
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Php Project 2</title>
     </head>
     <body>
+        
+        <?php 
+    echo 'sds';
+    $db = new mysqli("localhost", "phpTest", "o", "redcap");
+    if($db->connect_error) {
+        die("Connect Error ({$db->connect_errno}){$db->connect_error}");
+    }
+    /* $sql = "SELECT project_id, project_name, app_title FROM redcap.redcap_projects;"; */
+    $sql = "SELECT * FROM redcap.redcap_projects;"; 
+    $result = $db->query($sql);
+    echo $sql;
+    echo $result +'\n';
+    
+?>
+        
+        
         afjlkfadfdsafdsa
         <table cellSpacing="2" cellPaddin="6" align="center" border="1">
             <tr>
@@ -30,17 +40,11 @@ and open the template in the editor.
                 <td>Name</td>
                 <td>Title</td>
             </tr>
-            <?php while($row = $result->fetch_assoc()) {  ?>
-                <tr>
-                    <td><?php echo $row['project_id']; ?></td>
-                    <td><?php echo $row['project_name']; ?></td>
-                    <td><?php echo stripslashes($row['app_title']); ?></td>
-
-                </tr>
-            <?php}?>
+            
 
         </table>
 
+        
         
         <?php if(!empty($_POST['participantID'])) {
             echo "Greetings, Participant {$_POST['participantID']}, and welcome.";
